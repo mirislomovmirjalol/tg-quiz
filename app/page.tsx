65a0b93a688caf0f7d46dc648fac7b0ff300ce72
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2025-05-02 14:24:15 +0100

    refactor: use radio card instead of select

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,15 @@ import { Button } from "@/components/ui/button";
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
 import { redirect } from "next/navigation";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { useId } from "react";
+
 
 export default function Home() {
   return (
     <div className="container mx-auto p-4 max-w-2xl pt-16">
       <div className="text-center mb-8">
-        <h1 className="text-3xl font-bold mb-2">BrainBoost Quiz</h1>
+        <h1 className="text-3xl font-bold mb-2">JAI</h1>
       </div>
 
       <Card>
@@ -29,6 +32,8 @@ export default function Home() {
 
 
 function QuizForm() {
+  const id = useId()
+
   return (
     <Form action={async (formData) => {
       "use server"
@@ -56,18 +61,38 @@ function QuizForm() {
           />
         </div>
 
+        {/* use radio cards instead of select */}
         <div className="grid gap-2">
           <Label htmlFor="difficulty">Сложность</Label>
-          <Select name="difficulty" required>
-            <SelectTrigger className="w-full" id="difficulty">
-              <SelectValue placeholder="Выберите сложность" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="easy">Легко</SelectItem>
-              <SelectItem value="medium">Средне</SelectItem>
-              <SelectItem value="hard">Сложно</SelectItem>
-            </SelectContent>
-          </Select>
+          <RadioGroup className="grid-cols-3" defaultValue="easy" name="difficulty">
+            <div className="border-input has-data-[state=checked]:border-primary/50 has-focus-visible:border-ring has-focus-visible:ring-ring/50 relative flex cursor-pointer flex-col items-center gap-3 rounded-md border px-2 py-3 text-center shadow-xs transition-[color,box-shadow] outline-none has-focus-visible:ring-[3px]">
+              <RadioGroupItem id={`${id}-1`} value="easy" className="sr-only" />
+              <label
+                htmlFor={`${id}-1`}
+                className="text-foreground cursor-pointer text-xs leading-none font-medium after:absolute after:inset-0"
+              >
+                Легкий
+              </label>
+            </div>
+            <div className="border-input has-data-[state=checked]:border-primary/50 has-focus-visible:border-ring has-focus-visible:ring-ring/50 relative flex cursor-pointer flex-col items-center gap-3 rounded-md border px-2 py-3 text-center shadow-xs transition-[color,box-shadow] outline-none has-focus-visible:ring-[3px]">
+              <RadioGroupItem id={`${id}-2`} value="medium" className="sr-only" />
+              <label
+                htmlFor={`${id}-2`}
+                className="text-foreground cursor-pointer text-xs leading-none font-medium after:absolute after:inset-0"
+              >
+                Средний
+              </label>
+            </div>
+            <div className="border-input has-data-[state=checked]:border-primary/50 has-focus-visible:border-ring has-focus-visible:ring-ring/50 relative flex cursor-pointer flex-col items-center gap-3 rounded-md border px-2 py-3 text-center shadow-xs transition-[color,box-shadow] outline-none has-focus-visible:ring-[3px]">
+              <RadioGroupItem id={`${id}-3`} value="hard" className="sr-only" />
+              <label
+                htmlFor={`${id}-3`}
+                className="text-foreground cursor-pointer text-xs leading-none font-medium after:absolute after:inset-0"
+              >
+                Сложный
+              </label>
+            </div>
+          </RadioGroup>
         </div>
 
         <Button type="submit" size="lg" className="w-full">Создать викторину</Button>
